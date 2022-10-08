e6e9fbedbd1a73efbc6723aa0b3cc9d1712d4857
k.efimov <relict.adm@gmail.com>
2022-10-08 15:49:52 +0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const whitelist = ["http://localhost:8080"];
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    }
+  });
   await app.listen(3000);
 }
 bootstrap();
